ff95fd02b39a2554394e81cfe3c8cb2f3ae98ef0
Vittu Singh <vittu.singh@sparkeighteen.com>
2022-11-10 11:51:27 +0530

    update

--- JS Practice/main/index.js
+++ JS Practice/main/index.js
@@ -1,10 +1,10 @@
- {// *************  for in   *************
-// const obj={fname:"Vittu",lname:"Singh"};
-// for (let x in obj){
-//     console.log(x, obj[x]);      // x represents keys ,  obj[x] represents value of key x in obj
-// }
+{// *************  for in   *************
+    // const obj={fname:"Vittu",lname:"Singh"};
+    // for (let x in obj){
+    //     console.log(x, obj[x]);      // x represents keys ,  obj[x] represents value of key x in obj
+    // }
 
- }
+}
 
 
 {// *************  for of   ************
@@ -26,7 +26,7 @@
 }
 
 {// *************  label with continue abd break   *************
-    
+
     // loop1:
     // for (let i = 0; i < 3; i++) {     
     //     loop2:
@@ -38,75 +38,110 @@
     //     }
     // }
 
-   
+
 }
 
 
- { // *************  Maps  *************
+{ // *************  Maps  *************
 
-  /*  let map = new Map()
+    /*  let map = new Map()
+  
+      map.set("one", "first element");
+      map.set("two", "second element");
+      map.set(3, "third element");
+      console.log(map.get(3));    // returns value of key , here key given is 3 so it returns "third element"
+  
+      map.forEach(function(value, key) {
+          console.log(`${key}=${value}`);
+          console.log(typeof(key));
+          console.log(typeof(value));
+      })
+      console.log(map.keys());     //returns an iterator
+      console.log(map.values());     //returns an iterator
+      console.log(map.entries());     //returns an iterator
+  
+      for (const ent of map) {
+          
+          console.log(ent);
+      }
+      for (const [key,value] of map) {
+          console.log(key,value);
+  
+          // if(typeof(key) === "number"){
+          //     console.log(key.toString(),value);
+          // }else{
+  
+          //     console.log(key,value);
+          // }
+      }
+      for (const key of map.keys()) {
+          console.log(key);
+      }
+      for (const value of map.values()) {
+          console.log(value);
+      }
+      for (const entry of map.entries()) {
+          console.log(entry);
+      }
+  */
+}
 
-    map.set("one", "first element");
-    map.set("two", "second element");
-    map.set(3, "third element");
-    console.log(map.get(3));    // returns value of key , here key given is 3 so it returns "third element"
 
-    map.forEach(function(value, key) {
-        console.log(`${key}=${value}`);
-        console.log(typeof(key));
-        console.log(typeof(value));
-    })
-    console.log(map.keys());     //returns an iterator
-    console.log(map.values());     //returns an iterator
-    console.log(map.entries());     //returns an iterator
+{// **********    JSON     ************************* 
+    
 
-    for (const ent of map) {
-        
-        console.log(ent);
-    }
-    for (const [key,value] of map) {
-        console.log(key,value);
-
-        // if(typeof(key) === "number"){
-        //     console.log(key.toString(),value);
-        // }else{
-
-        //     console.log(key,value);
-        // }
-    }
-    for (const key of map.keys()) {
-        console.log(key);
-    }
-    for (const value of map.values()) {
-        console.log(value);
-    }
-    for (const entry of map.entries()) {
-        console.log(entry);
-    }
-*/
- }
-
-
- {
-    // **********    JSON     *************************
-     
     // creating a javascript string containing JSON syntax
-    let text = '{ "employees" : [' +                            
-    '{ "firstName":"John" , "lastName":"Doe" },' +
-    '{ "firstName":"Anna" , "lastName":"Smith" },' +
-    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+    // let text = '{ "employees" : [' +                            
+    // '{ "firstName":"John" , "lastName":"Doe" },' +
+    // '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    // '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
     // Then, using the JavaScript built-in function JSON.parse() to convert the string into a JavaScript object
 
-    let obj=JSON.parse(text);    // here obj is now a normal JS object
+    // let obj=JSON.parse(text);    // here obj is now a normal JS object
 
     // then use  this object as used normally in javascript
-    console.log(obj);
-    
- }
+    // console.log(obj);
+
+}
 
 
- 
+{       // **********   simple Counter ****************
+
+   
+    let counter = 0;
+    document.getElementById("CounterDisplay").textContent = counter;
+    document.getElementById("inc").addEventListener("click", increment);
+    document.getElementById("dec").addEventListener("click", decrement);
+    document.getElementById("reset").addEventListener("click", reset);
+
+    function increment() {
+        if (counter === 10) {
+            window.alert("Maximum Limit Reached");
+            return;
+        }
+        counter += 1;
+        display();
+    }
+    function decrement() {
+        if (counter < 1) { 
+            window.alert("Minimum Limit Reached");
+            return; }
+        counter -= 1;
+        display();
+    }
+    function reset() {
+        counter = 0;
+        display();
+    }
+    function display() {
+        document.getElementById("CounterDisplay").textContent = counter;
+    }
+
+}
+
+
+
 
 
 
