75cfc45b0bf80520d09ccbeb4545205850080b2b
Vittu Singh <vittu.singh@sparkeighteen.com>
2022-12-16 11:13:37 +0530

    tested input type file

--- JS Practice/main/index.js
+++ JS Practice/main/index.js
@@ -570,3 +570,15 @@
     //     }
     // })
 }
+
+const imgInput = document.querySelector('input')
+const imgEl = document.querySelector('img')
+imgInput.addEventListener('change', () => {
+  if (imgInput.files && imgInput.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      imgEl.src = e.target.result;
+    }
+    reader.readAsDataURL(imgInput.files[0]);
+  }
+})
