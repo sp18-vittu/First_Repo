b67a30d440a91d6de53bbc5f342b8cc845ba2c02
Vittu Singh <vittu.singh@sparkeighteen.com>
2023-02-01 10:51:54 +0530

    refactor

--- JS Practice/main/index.js
+++ JS Practice/main/index.js
@@ -570,15 +570,17 @@
     //     }
     // })
 }
+{   //for showing image after upload  
 
-const imgInput = document.querySelector('input')
-const imgEl = document.querySelector('img')
-imgInput.addEventListener('change', () => {
-  if (imgInput.files && imgInput.files[0]) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      imgEl.src = e.target.result;
-    }
-    reader.readAsDataURL(imgInput.files[0]);
-  }
-})
+// const imgInput = document.querySelector('input')
+// const imgEl = document.querySelector('img')
+// imgInput.addEventListener('change', () => {
+//   if (imgInput.files && imgInput.files[0]) {
+//     const reader = new FileReader();
+//     reader.onload = (e) => {
+//       imgEl.src = e.target.result;
+//     }
+//     reader.readAsDataURL(imgInput.files[0]);
+//   }
+// })
+}
